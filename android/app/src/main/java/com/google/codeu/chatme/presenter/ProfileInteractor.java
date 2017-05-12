70eb36dcbac353b7e21b5a4ddadfaf8dae85af4a
Yash Mittal <yashmittal2009@gmail.com>
2017-05-12 11:05:40 -0400

    Fixed #29 (log out back btn)

--- android/app/src/main/java/com/google/codeu/chatme/presenter/ProfileInteractor.java
+++ android/app/src/main/java/com/google/codeu/chatme/presenter/ProfileInteractor.java
@@ -2,6 +2,8 @@ package com.google.codeu.chatme.presenter;
 
 import android.net.Uri;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 /**
  * This interface provides functions which primarily relate to login
  * and sign up functionality of this Firebase-powered chat application
@@ -39,4 +41,16 @@ public interface ProfileInteractor {
      * @param data {@link Uri} containing new profile picture
      */
     void uploadProfilePictureToStorage(Uri data);
+
+    /**
+     * Adds {@link com.google.firebase.auth.FirebaseAuth.AuthStateListener} to
+     * {@link FirebaseAuth} which is the entry point to Firebase SDK
+     */
+    public void setAuthStateListener();
+
+    /**
+     * Removes {@link com.google.firebase.auth.FirebaseAuth.AuthStateListener} from
+     * {@link FirebaseAuth} if it exists
+     */
+    public void removeAuthStateListener();
 }
