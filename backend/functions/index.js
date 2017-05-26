933150885304f215805c2afec02bf644dc47ef66
Yash Mittal <yashmittal2009@gmail.com>
2017-05-26 04:05:38 -0400

    Basic notification in place when app not in focus

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -79,3 +79,52 @@ exports.setUniqueUsername = functions.auth.user().onCreate(event => {
 		return ref.child(id).child("username").set(uniqueUsername);
 	});
 });
+
+exports.sendNotificationOnNewMessage = functions.database.ref("/messages/{messageId}")
+	.onWrite(event => {
+		const snapshot = event.data;
+		// only send a notification when a new message has been created
+		if (snapshot.previous.val()) {
+			return;
+		}
+
+		const message = snapshot.val();
+		var ref = admin.database().ref("conversations");
+
+		ref.child(message.conversation).child("participants").once("value", function(snapshot) {
+			var participants = snapshot.val();
+			// do not send notification to self
+			const authorIdx = participants.indexOf(message.author);
+			participants.splice(authorIdx, 1);
+
+			var promises = [];
+			// creates promises to retrieve tokens of users to send notification to
+			for (var i = 0; i < participants.length; ++i) {
+				const promise = admin.database().ref(`/users/${participants[i]}/instanceId`).once('value');
+				promises.push(promise);
+			}
+
+			admin.database().ref("users").child(message.author).child("fullName")
+				.once("value", function(snapshot) {
+					const payload = {
+						notification: {
+							title: `${snapshot.val()} sent a message`,
+							body: message.content
+						}
+					}
+
+					return Promise.all(promises).then(results => {
+						var tokens = [];
+						for (var i = 0; i < results.length; ++i) {
+							const token = results[i].val();
+							if (token.length > 0) {
+								tokens.push(token);
+							}
+						}
+
+						// pushes notification to device(s)
+						admin.messaging().sendToDevice(tokens, payload);
+					});
+				});
+		});
+	});
